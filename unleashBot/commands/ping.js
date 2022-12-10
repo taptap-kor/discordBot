1ecba81ee1e36b4f1cfaa63b0fbe32b3f5849515
taptap-kor <tyy8282@naver.com>
2022-12-10 23:01:18 +0900

    fix: wait time changed

--- unleashBot/commands/ping.js
+++ unleashBot/commands/ping.js
@@ -2,20 +2,23 @@ const { SlashCommandBuilder } = require('discord.js');
 const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
-	data : new SlashCommandBuilder()
-	.setName('lazymint')
-	.setDescription('input token id')
-	.addStringOption(option =>
-		option.setName('token_id')
-			.setDescription('Plz input tokenId you wanna mint')
-			.setRequired(true)),
+  data: new SlashCommandBuilder()
+    .setName('lazymint')
+    .setDescription('input token id')
+    .addStringOption(option =>
+      option
+        .setName('token_id')
+        .setDescription('Plz input tokenId you wanna mint')
+        .setRequired(true)
+    ),
 
-	async execute(interaction) {
-		const tokenId = interaction.options.getString('token_id') ?? 'No reason provided';
-		await interaction.reply(`Minting Token... (ID: ${tokenId}) `);
-		console.log("minting...");
-		await wait(500);
-		await interaction.deleteReply();
-		return tokenId;
-	}
+  async execute(interaction) {
+    const tokenId =
+      interaction.options.getString('token_id') ?? 'No reason provided';
+    await interaction.reply(`Minting Token... (ID: ${tokenId}) `);
+    console.log('minting...');
+    await wait(100);
+    await interaction.deleteReply();
+    return tokenId;
+  },
 };
